c1176af9ee67ff3fc9f5eb8e24f78d57f22a48b5
Rohitdaddekar <rohitdaddekar24@gmail.com>
2021-12-21 16:45:57 +0530

    new project added

---
+++ setup.sh
@@ -0,0 +1,18 @@
+<<<<<<< HEAD
+mkdir -p ~/.streamlit/
+echo "\
+[server]\n\
+headless = true\n\
+port = $PORT\n\
+enableCORS = false\n\
+\n\
+=======
+mkdir -p ~/.streamlit/
+echo "\
+[server]\n\
+headless = true\n\
+port = $PORT\n\
+enableCORS = false\n\
+\n\
+>>>>>>> 56b0e6a3245ecbe841e93355d740339597a48b5b
+" > ~/.streamlit/config.toml
